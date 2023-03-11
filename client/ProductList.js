8a9707174c122f27f23bd025088325e1ae860368
petro-mykhats2 <02webproject02@gmail.com>
2023-03-11 15:48:23 +0100

    test AddProducts - local works

--- client/ProductList.js
+++ client/ProductList.js
@@ -1,23 +1,27 @@
 import React from "react"
-// import { withTracker } from "meteor/react-meteor-data"
-// import { Products } from "../api/products"
+import { withTracker } from "meteor/react-meteor-data"
+import { Products } from "../imports/api/products"
 
-const ProductList = () => (
+const ProductList = ({ products }) => (
   <div>
     <h2>Product List</h2>
-    {/* <ul>
-      {products.map((product) => (
-        <li key={product._id}>
-          {product.name} - {product.price}
-        </li>
-      ))}
-    </ul> */}
+    {products && products.length ? (
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p>No products found</p>
+    )}
   </div>
 )
-export default ProductList
-// export default withTracker(() => {
-//   Meteor.subscribe("products")
-//   return {
-//     products: Products.find().fetch(),
-//   }
-// })(ProductList)
+
+export default withTracker(() => {
+  Meteor.subscribe("products")
+  return {
+    products: Products.find().fetch(),
+  }
+})(ProductList)
