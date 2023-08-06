53f2666e93aab279ada24eacbbffd88d0dfe5cc2
petro-mykhats2 <02webproject02@gmail.com>
2023-08-06 15:13:46 +0200

    test commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,7 @@ import { Info } from './Info.jsx';
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <h2>06.08.2023 test chenged</h2>
     <Hello/>
     <Info/>
     <AddProduct />
